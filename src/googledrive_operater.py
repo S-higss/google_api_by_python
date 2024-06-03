72bf529ffe6b98dadad4504d90237478b0b8d4f2
S-higss <shonoji2001@gmail.com>
2024-06-03 16:17:07 +0900

    [update]for release

--- src/googledrive_operater.py
+++ src/googledrive_operater.py
@@ -1,9 +1,4 @@
-from __future__ import print_function
-
-import os.path, sys, mimetypes
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+import mimetypes
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
